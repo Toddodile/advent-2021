bba4e3ebf2fa1b2b681d4e5e56cfc261082f088c
Todd <todd5913@vt.edu>
2021-12-06 12:04:44 -0500

    cleanup day 1 and day 2 done

--- day1/SeaDepth.cs
+++ day1/SeaDepth.cs
@@ -6,18 +6,12 @@ class TestClass
     static void Main(string[] args)
     {
         string[] lines = File.ReadAllLines("inputs.txt");
-        int a = int.Parse(lines[0]);
-        int b = int.Parse(lines[1]);
-        int c = int.Parse(lines[2]);
         int increases = 0;
         for (int i = 3; i < lines.Length; i++) {
             int currentDepth = int.Parse(lines[i]);
-            if (currentDepth > a) {
+            if (int.Parse(lines[i]) > int.Parse(lines[i-3])) {
                 increases++;
             }
-            a = b;
-            b = c;
-            c = currentDepth;
         }
         // Display the number of command line arguments.
         Console.WriteLine("Increases: {0}", increases);
---
+++ day2/Pathing.cs
@@ -0,0 +1,28 @@
+using System;
+using System.IO;
+
+class TestClass
+{
+    static void Main(string[] args)
+    {
+        int x = 0;
+        int y = 0;
+        int aim = 0;
+        string[] directions = File.ReadAllLines("inputs.txt");
+        
+        for (int i = 0; i < directions.Length; i++) {
+            string[] instruction = directions[i].Split(' ');
+            if (instruction[0].Equals("forward")) {
+                x += int.Parse(instruction[1]);
+                y += int.Parse(instruction[1]) * aim;
+            } else if (instruction[0].Equals("down")) {
+                aim += int.Parse(instruction[1]);
+            } else {
+                aim -= int.Parse(instruction[1]);
+            }
+        }
+
+        Console.WriteLine("Position: ({0}, {1}) at heading {2}", x, y, aim);
+        Console.WriteLine("Product: {0}", x * y);
+    }
+}
