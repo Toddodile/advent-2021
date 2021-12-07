1d62af11e20edafd9c0e8f17dfdebf9c699d13ff
Todd <todd5913@vt.edu>
2021-12-07 18:02:45 -0500

    add template folder

---
+++ template/Template.cs
@@ -0,0 +1,14 @@
+using System;
+using System.IO;
+
+class Template
+{
+    static void Main(string[] args)
+    {
+        string[] inputs = File.ReadAllLines("inputs.txt");
+        
+        for (int i = 0; i < inputs.Length; i++) {
+            // Do something probably
+        }
+    }
+}
