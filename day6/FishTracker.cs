18f9f249172712a7ec776466eea7f2657e432fe6
Todd <todd5913@vt.edu>
2021-12-08 19:02:45 -0500

    day 6 template created

--- day6/FishTracker.cs
+++ day6/FishTracker.cs
@@ -1,11 +1,12 @@
 using System;
 using System.IO;
 
-class Template
+class FishTracker
 {
     static void Main(string[] args)
     {
         string[] inputs = File.ReadAllLines("test.txt");
+        string[] initialPopulation = inputs[0].Split(",");
         
         for (int i = 0; i < inputs.Length; i++) {
             // Do something probably
